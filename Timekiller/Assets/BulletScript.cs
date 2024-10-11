b7c8a690a164790ec4916805c2da460515085e30
Projecttea <projecttea123@gmail.com>
2024-10-11 10:35:53 +0200

    Added Shooting

---
+++ Timekiller/Assets/BulletScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 dir = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(dir.x, dir.y).normalized * force;
+
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
