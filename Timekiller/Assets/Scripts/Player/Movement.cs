1670e07a71cc84d5241e80e6f2b0eea6b3d6ee64
Projecttea <projecttea123@gmail.com>
2024-10-11 09:50:15 +0200

    Added Player that can look around

---
+++ Timekiller/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    Camera cam;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float camDis = cam.transform.position.y - playerTransform.position.y;
+        Vector3 mouse = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDis));
+
+        float AngleRad = Mathf.Atan2(mouse.y - playerTransform.position.y, mouse.x - playerTransform.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+
+        rb.rotation = angle - 90;
+    }
+}
