bbc7462bceb04108efe4b6816b568e26aca97f70
ThijsHeuvel <d294396@edu.curio.nl>
2024-11-21 11:44:02 +0100

    Made bullets actually despawn after a certain time

--- Timekiller/Assets/Scripts/Attacking/BulletScript.cs
+++ Timekiller/Assets/Scripts/Attacking/BulletScript.cs
@@ -8,6 +8,7 @@ public class BulletScript : MonoBehaviour
     private Camera mainCam;
     private Rigidbody2D rb;
     public float force;
+    public float lifeTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,10 @@ public class BulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
